24229ca9bc63731cef1f8bf3c60751548f05f47d
ECR\Egon Rasmussen <ecr@live.dk>
2020-02-01 11:14:32 +0100

    ok

--- RealEstateApp/RealEstateApp/Models/PropertyListItem.cs
+++ RealEstateApp/RealEstateApp/Models/PropertyListItem.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace RealEstateApp.Models
 {
+    // Her kunne vi også have benyttet Fody og blot tilføjet følgende property: [AddINotifyPropertyChangedInterface]
     public class PropertyListItem : INotifyPropertyChanged
     {
         public PropertyListItem(Property property)
