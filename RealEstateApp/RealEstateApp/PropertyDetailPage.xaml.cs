02664a8f2a3743f7b8454b5eaa6139e4dd4e9581
Egon C. Rasmussen <ecr@eucsyd.dk>
2020-09-01 11:06:41 +0200

    ok

--- RealEstateApp/RealEstateApp/PropertyDetailPage.xaml.cs
+++ RealEstateApp/RealEstateApp/PropertyDetailPage.xaml.cs
@@ -14,21 +14,12 @@ namespace RealEstateApp
         {
             InitializeComponent();
 
-            Property = propertyListItem.Property;
 
-            IRepository Repository = TinyIoCContainer.Current.Resolve<IRepository>();
-            Agent = Repository.GetAgents().FirstOrDefault(x => x.Id == Property.AgentId);
-
-            BindingContext = this;
         }
 
-        public Agent Agent { get; set; }
-
-        public Property Property { get; set; }
 
         private async void EditProperty_Clicked(object sender, System.EventArgs e)
         {
-            await Navigation.PushAsync(new AddEditPropertyPage(Property));
-        }
+
     }
 }
--- RealEstateApp/RealEstateApp/PropertyListPage.xaml.cs
+++ RealEstateApp/RealEstateApp/PropertyListPage.xaml.cs
@@ -19,43 +19,34 @@ namespace RealEstateApp
             InitializeComponent();
 
             Repository = TinyIoCContainer.Current.Resolve<IRepository>();
-            LoadProperties();
-            BindingContext = this; 
+
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
 
-            LoadProperties();
+
         }
 
         void OnRefresh(object sender, EventArgs e)
         {
-            var list = (ListView)sender;
-            LoadProperties();
-            list.IsRefreshing = false;
+
         }
 
         void LoadProperties()
         {
-            PropertiesCollection.Clear();
-            var items = Repository.GetProperties();
 
-            foreach (Property item in items)
-            {
-                PropertiesCollection.Add(new PropertyListItem(item));
-            }
         }
 
         private async void ItemsListView_ItemTapped(object sender, ItemTappedEventArgs e)
         {
-            await Navigation.PushAsync(new PropertyDetailPage(e.Item as PropertyListItem));
+
         }
 
         private async void AddProperty_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new AddEditPropertyPage());
+
         }    
     }
 }
